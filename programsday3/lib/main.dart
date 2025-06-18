9ae1afbd75e0c5ea7ae7d0738b401e76e29d4303
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-06-18 10:40:28 +0530

    Projects

---
+++ programsday3/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project day 6',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red)
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text("Programs"),
+        ),
+      ),
+    );
+  }
+}
